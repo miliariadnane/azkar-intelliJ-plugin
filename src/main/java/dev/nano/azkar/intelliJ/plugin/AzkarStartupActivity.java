9976e1bc362e09ed7c2f7938ce7e1edabbc663e2
miliariadnane <miliari.adnane@gmail.com>
2022-11-15 22:10:32 +0100

    read azkar from json file

--- src/main/java/dev/nano/azkar/intelliJ/plugin/AzkarStartupActivity.java
+++ src/main/java/dev/nano/azkar/intelliJ/plugin/AzkarStartupActivity.java
@@ -1,23 +1,44 @@
 package dev.nano.azkar.intelliJ.plugin;
 
 import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import dev.nano.azkar.intelliJ.plugin.dataSource.ReadData;
+import dev.nano.azkar.intelliJ.plugin.dataSource.bean.Zikr;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.List;
 
 public class AzkarStartupActivity implements StartupActivity {
 
     @Override
     public void runActivity(@NotNull Project project) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
 
+        /*
         new Notification(
                 "dev.nano.azkar.intelliJ.plugin.notificationGroup",
                 "Azkar",
                 "Hello World!",
                 NotificationType.INFORMATION
         ).notify(project);
+        */
+
+        try {
+            List<Zikr> zikrList = ReadData.getAzkarFromJson();
+            NotificationGroupManager.getInstance()
+                    .getNotificationGroup("dev.nano.azkar.intelliJ.plugin.notificationGroup")
+                    // get first zikr from json files
+                    .createNotification(
+                            zikrList.get(0).getZikr(),
+                            NotificationType.INFORMATION
+                    ).notify(project);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/main/java/dev/nano/azkar/intelliJ/plugin/dataSource/ReadData.java
+++ src/main/java/dev/nano/azkar/intelliJ/plugin/dataSource/ReadData.java
@@ -3,7 +3,6 @@ package dev.nano.azkar.intelliJ.plugin.dataSource;
 import dev.nano.azkar.intelliJ.plugin.dataSource.bean.Zikr;
 import org.apache.commons.io.IOUtils;
 import com.google.common.collect.ImmutableList;
-import com.google.common.io.Resources;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import java.io.IOException;
@@ -11,10 +10,12 @@ import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
 public class ReadData {
 
     public static ImmutableList<Zikr> getAzkarFromJson() throws IOException {
-        InputStream inputStream = Resources.getResource("azkar.json").openStream();
+        InputStream inputStream = ReadData.class.getClassLoader().getResourceAsStream("azkar.json");
+        //InputStream inputStream = Resources.getResource("data/azkar.json").openStream();
         String json = IOUtils.toString(inputStream);
         Type listType = new TypeToken<ArrayList<Zikr>>() {
         }.getType();
